5814d310786d6d92e0cccf66984d2473f477f42c
jarlen <jarlen_china@sina.com>
2016-11-14 13:13:41 +0800

    添加应用升级模块

--- app/src/main/java/cn/jarlen/houseinspection/UIApplication.java
+++ app/src/main/java/cn/jarlen/houseinspection/UIApplication.java
@@ -3,6 +3,7 @@ package cn.jarlen.houseinspection;
 import android.app.Application;
 
 import com.baidu.mapapi.SDKInitializer;
+import com.tencent.bugly.Bugly;
 
 import cn.sharesdk.framework.ShareSDK;
 
@@ -17,5 +18,6 @@ public class UIApplication extends Application {
         super.onCreate();
         SDKInitializer.initialize(getApplicationContext());
         ShareSDK.initSDK(getApplicationContext());
+        Bugly.init(getApplicationContext(),"177cf5e319",false);
     }
 }
